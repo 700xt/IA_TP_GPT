2a046702d50d5c77973ccc65a5b7ae7eaedda33b
Your Name <you@example.com>
2025-05-16 13:26:57 +0200

    Question3

--- SystemeIntelligentAvancer.py
+++ SystemeIntelligentAvancer.py
@@ -15,3 +15,20 @@ for i in range(NUMBER):
     for j in range(NUMBER):
         X.append([i, j])
 X = np.array(X)
+
+#Question 3 : Générer la matrice
+y = []
+
+
+for couple in X:
+    x = couple[0]
+    y_val = couple[1]
+    produit = x * y_val
+#Vérifie la parité
+    if produit % 2 == 1:
+        y.append(1)  # impair
+    else:
+        y.append(0)  # pair
+
+#Convertir en tableau numpy
+y = np.array(y)
