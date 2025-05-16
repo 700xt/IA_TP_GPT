510896c304595f6ec429858e506e789a8cdb456e
Thomas Grenet <thomas.grenet@iut-dijon.u-bourgogne.fr>
2025-05-16 13:20:50 +0200

    Question 1

---
+++ SystemeIntelligentAvancer.py
@@ -0,0 +1,7 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+
+NUMBER = 100
