0b416bd481391fcb394ea3d9690d1ce8451c207d
Your Name <you@example.com>
2025-05-16 14:20:31 +0200

    Question9

--- SystemeIntelligentAvancer.py
+++ SystemeIntelligentAvancer.py
@@ -1,4 +1,5 @@
 # Question 1 : Librairie
+
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -10,6 +11,7 @@ NUMBER = 100
 
 
 # Question 2 : Création matrice 
+
 X = []
 for i in range(NUMBER):
     for j in range(NUMBER):
@@ -17,6 +19,7 @@ for i in range(NUMBER):
 X = np.array(X)
 
 #Question 3 : Générer la matrice
+
 y = []
 
 
@@ -34,6 +37,7 @@ for couple in X:
 y = np.array(y)
 
 #Question 4 : Séparation ensemble de test et d'entrainement
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #Question 5 : Instancier un réseau de neurone
@@ -45,7 +49,18 @@ modele = MLPClassifier(hidden_layer_sizes=(15,), max_iter=1000, random_state=42)
 modele.fit(X_train,y_train)
 
 # Question 7 : Affichage le nombre d'époques (42)
+
 print(f"Convergence en {modele.n_iter_} époques")
 
 # Question 8 : Evaluation du réseau de neurone sur l'ensemble de test
+
 y_pred = modele.predict(X_test)
+
+# Question 9 : Calculer l'exactitude et l'indiquer en commentaire
+
+accuracy = accuracy_score(y_test,y_pred)
+print(f"Exactitude : {accuracy:.2f}")
+
+
+
+
