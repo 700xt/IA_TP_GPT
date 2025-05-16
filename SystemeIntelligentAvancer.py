40f4dfd437550076399a5599e9553dc0ccfdad68
DckoGwendal <gwendal.dececco@gmail.com>
2025-05-16 13:22:41 +0200

    Question 2 : Matrice

--- SystemeIntelligentAvancer.py
+++ SystemeIntelligentAvancer.py
@@ -1,7 +1,17 @@
+# Question 1 : Librairie
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 import numpy as np
 
+# Variable
 NUMBER = 100
+
+
+# Question 2 : Création matrice 
+X = []
+for i in range(NUMBER):
+    for j in range(NUMBER):
+        X.append([i, j])
+X = np.array(X)
